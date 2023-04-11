89c7cb49f8c487c4cb5dc1b08883655082d818c4
Connor Swanson <swanscon@gmail.com>
2023-04-11 16:07:40 -0500

    Changes to vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/er-loadcalc/',
+  base: '/loadcalc/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
